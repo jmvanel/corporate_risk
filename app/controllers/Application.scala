7b5c8d6f50060a402f12aff75cac9ad4aced1ae4
Sylvain <lebon.sylvain@gmail.com>
2015-01-13 00:18:53 +0100

    feature: form page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,20 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import deductions.runtime.html.CreationForm
 
 object Application extends Controller {
 
   def index = Action {
       Ok(views.html.index("Application"))
+  }
+  
+  def form = Action {
+      Ok(views.html.form(new CreationForm { actionURI = "/save" }.create("http://xmlns.com/foaf/0.1", "en").get))
+  }
+  
+  def save = Action {
+      Ok(views.html.index("Merci pour ce formulaire !"))
   }
 
 }
