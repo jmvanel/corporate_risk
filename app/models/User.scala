d797b6ef76431a5aa36d490fbfbe7c6675820363
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2015-01-16 22:59:43 +0100

    User.scala implementation ready for use

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,34 +1,49 @@
 package models
 
-import play.api._
-import org.w3.banana._
-import org.w3.banana.jena.JenaModule
-import java.security.MessageDigest
-import deductions.runtime.jena.RDFStoreObject
 import scala.util.Try
+
+import org.w3.banana.Prefix
+import org.w3.banana.RDF
+import org.w3.banana.RDFOps
+import org.w3.banana.RDFOpsModule
+import org.w3.banana.RDFStore
 import org.w3.banana.jena.Jena
+import org.w3.banana.jena.JenaModule
+
+import deductions.runtime.jena.RDFStoreObject
+
+import java.security.MessageDigest
 
 /** TODO indiquer le but de la classe */
-// trait RDFUser extends RDFModule with RDFOpsModule {
 abstract class RDFUser[Rdf <: RDF](implicit ops: RDFOps[Rdf],
-    rdfStore: RDFStore[Rdf, Try, RDFStoreObject.DATASET]) {
+    rdfStore: RDFStore[Rdf, Try, RDFStoreObject.DATASET]) //    extends UserVocab
+    {
   val rdfStoreObject = RDFStoreObject
   import ops._
-  import RDFStoreUser._
-  val bizinnovUserGraphURI = URI("http://bizinnov.com/ontologies/users/")
 
-  def checkPassword(password: String): Boolean = {
-    val passwordDigest = MessageDigest.getInstance("MD5").digest(password.getBytes)
-    val passwordFromRDFStore = "" // TODO <<<<<<<<<<<<<<<
+  // TODO duplicate with UserVocab
+  val bizinnovUserPrefix = Prefix("usr", "http://bizinnov.com/ontologies/users/")
+  val bizinnovUserGraphURI = URI(bizinnovUserPrefix.prefixIri)
+  val bizinnovUserVocabPrefix = Prefix("user", "http://bizinnov.com/ontologies/users.owl.ttl#")
+
+  def checkPassword(fromKeyboard: String, user: User): Boolean = {
+    val passwordDigest = MessageDigest.getInstance("MD5").digest(fromKeyboard.getBytes)
+    val passwordFromRDFStore = user.password
     passwordFromRDFStore == passwordDigest
   }
 
   def save(user: User) = {
     val triples = List(
       makeTriple(
-        makeUri(bizinnovUserVocabPrefix + user.email),
-        makeUri(bizinnovUserVocabPrefix + "password"),
-        makeUri(bizinnovUserVocabPrefix + MessageDigest.getInstance("MD5").digest(user.password.getBytes))))
+        bizinnovUserPrefix(user.email),
+        bizinnovUserVocabPrefix("password"),
+        makeLiteral(MessageDigest.getInstance("MD5").digest(user.password.getBytes)
+          .toString(), xsd.string)),
+      makeTriple(
+        bizinnovUserPrefix(user.email),
+        bizinnovUserVocabPrefix("email"),
+        makeLiteral(user.email, xsd.string)
+      ))
     val graph = makeGraph(triples)
     rdfStoreObject.rdfStore.rw(rdfStoreObject.dataset, {
       rdfStore.appendToGraph(rdfStoreObject.dataset, bizinnovUserGraphURI, graph)
@@ -37,25 +52,42 @@ abstract class RDFUser[Rdf <: RDF](implicit ops: RDFOps[Rdf],
 }
 
 /** TODO indiquer le but de la classe */
-class User(var email: String, var password: String)
+case class User(var email: String, var password: String)
   extends RDFUser[Jena]
 
-/** User lookup, using RDF store  */
-object RDFStoreUser extends JenaModule {
+/** gather URI's and prefixes for user management */
+trait UserVocab extends RDFOpsModule {
   import ops._
-  val bizinnovUserVocabPrefix = "http://bizinnov.com/ontologies/users.owl.ttl#"
-  val bizinnovUserGraphURI = URI("http://bizinnov.com/ontologies/users/")
+  val bizinnovUserPrefix = Prefix("usr",
+    "http://bizinnov.com/ontologies/users/")
+  val bizinnovUserGraphURI = URI(bizinnovUserPrefix.prefixIri)
+  val bizinnovUserVocabPrefix = Prefix("user",
+    "http://bizinnov.com/ontologies/users.owl.ttl#")
+}
+
+/** User lookup, using RDF store  */
+object RDFStoreUser extends JenaModule with UserVocab {
+  import ops._
   val rdfStoreObject = RDFStoreObject
+
   def find(email: String): Option[User] = {
-    rdfStoreObject.rdfStore.r(rdfStoreObject.dataset, {
-      val userGraph = rdfStore.getGraph(rdfStoreObject.dataset, bizinnovUserGraphURI).get
-      // TODO null pas beau !!!!!!!!!!!!!!
-      val triples = ops.getObjects(userGraph, URI(bizinnovUserVocabPrefix + email), null)
-    })
-    //    if( triples.size > 0 )
-    //      Some(???)
-    //     else
-    None // TODO <<<<<<<<<
+    val user = rdfStoreObject.rdfStore.r(
+      rdfStoreObject.dataset, {
+        val userGraph = rdfStore.getGraph(rdfStoreObject.dataset, bizinnovUserGraphURI).get
+        val userURI = getSubjects(userGraph,
+          bizinnovUserVocabPrefix("email"),
+          bizinnovUserPrefix(email))
+        if (!userURI.isEmpty) {
+          val password = getObjects(userGraph, userURI.head,
+            bizinnovUserVocabPrefix("password"))
+          val email = getObjects(userGraph, userURI.head,
+            bizinnovUserVocabPrefix("email"))
+          if (!password.isEmpty) {
+            Some(User(email.head.toString(), password.head.toString()))
+          } else None
+        } else None
+      })
+    user.getOrElse(None)
   }
 }
 
