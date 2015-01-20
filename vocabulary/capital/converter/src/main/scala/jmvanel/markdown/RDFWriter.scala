0a4b304284b664c0a7cfe561701743ffecc60688
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2015-01-20 11:28:32 +0100

    markdown/RDFWriter.scala : gérer titres paragraphes Markdown (headers)
    TODO gérer texte de paragraphe

--- vocabulary/capital/converter/src/main/scala/jmvanel/markdown/RDFWriter.scala
+++ vocabulary/capital/converter/src/main/scala/jmvanel/markdown/RDFWriter.scala
@@ -4,8 +4,6 @@ The utility of plain text is mostly to get the text without any markup, useful
 in cases where you're doing something like pulling the header into a title
 field, etc.
 
-**TODO** HTML is currently bypassed, we should parse it and strip out the text.
-
 */
 
 package jmvanel.markdown
@@ -14,11 +12,18 @@ import java.io.{ StringWriter, Writer }
 import com.tristanhunt.knockoff._
 
 trait RDFWriter {
-  val prefix = "http://www.bizinnov.com/ontologies/quest.owl.ttl#"
+  val prefix = "ques:" // http://www.bizinnov.com/ontologies/quest.owl.ttl#"
   var index=0
   /** Creates a Group representation of the document. */
-  def toTTL( blocks : Seq[Block] ) : String = {
+  def toTTL(blocks: Seq[Block]): String = {
     implicit val writer = new StringWriter
+    if (index == 0) writer.write("""
+      @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.
+      @prefix owl:  <http://www.w3.org/2002/07/owl#>.
+      @prefix ques: <http://www.bizinnov.com/ontologies/quest.owl.ttl#> .
+      @prefix     : <http://www.bizinnov.com/ontologies/quest.owl.ttl#> .
+      """)
+
     blocksToTTL( blocks )
     writer.toString
   }
@@ -29,7 +34,11 @@ trait RDFWriter {
   def blockToTTL( block : Block )( implicit writer : Writer ) : Unit = {
     block match {
       case Paragraph( spans, _ ) => spans.foreach( spanToTTL )
-      case Header( _, spans, _ ) => spans.foreach( spanToTTL )
+//      case Header( _, spans, _ ) => 
+      case h:Header => 
+        updatecurrentURI()
+        headerToTTL(h, currentURI() )
+//        spans.foreach( spanToTTL )
       case LinkDefinition( _, _, _, _ ) => {}
       case Blockquote( children, _ ) => children.foreach( blockToTTL )
       case CodeBlock( text, _ ) => writer.write( text.content )
@@ -38,23 +47,39 @@ trait RDFWriter {
       case UnorderedItem( children, _ ) => children.foreach( blockToTTL )
       case OrderedList( items ) => items.foreach( blockToTTL )
       case UnorderedList( items ) => {
-        index=index+1
-        val uri = prefix + index
+        updatecurrentURI()
+        val uri = currentURI()
         items.foreach( it => itemToTTL(it, uri) )
       }
     }
     writer.write(" ")
   }
   
-  def itemToTTL( item: UnorderedItem, uri: String )( implicit writer : Writer ) : Unit = {
-    val head = item.children.head
-    head match {
-      case Paragraph( Seq(Text(t)), _ ) =>
-        writer.write( s"""<$uri> rdfs:label "${t.replaceFirst("\n$", "")}" .\n""")        
+  def currentURI() = prefix + index
+  def updatecurrentURI() = index=index+1 
+     
+  def headerToTTL(h:Header, uri: String="") ( implicit writer : Writer ) = {
+    h match {
+      case Header(_,List(Text(t)),_) => 
+      // Header(1,List(Text(T1)),1.1)
+        writeTriple(uri, prefix+"header", t)
       case _ =>
     }
   }
   
+  def itemToTTL( item: UnorderedItem, uri: String )( implicit writer : Writer ) : Unit = {
+    val head = item.children.head
+    head match {
+      case Paragraph( Seq(Text(t)), _ ) =>
+      writeTriple(uri, "rdfs:label", t)
+      case _ =>
+    }
+  }
+  
+  /** write a literal Triple; subject and pred are with an Turtle prefix */
+  def writeTriple(subject:String, pred:String, objet:String ) ( implicit writer : Writer ) =
+    writer.write( s"""$subject $pred "${objet.replaceFirst("\n$", "")}" .\n""")        
+
   def spanToTTL( span : Span )( implicit writer : Writer ) : Unit = {
     span match {
       case Text( content ) => writer.write( content )
