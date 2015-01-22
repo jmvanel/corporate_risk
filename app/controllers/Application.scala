4d1092d9c658982871d288ffd13c43a1b9e40c3c
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2015-01-22 22:43:28 +0100

    new object LinksToUserForms; /form route now edits given URI instance

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,21 +6,34 @@ import deductions.runtime.html.{ CreationForm, TableView }
 import Auth._
 
 object Application extends Controller with Secured {
+  lazy val tableView = new TableView {}
 
   def index = withAuth { username =>
     implicit request =>
       Ok(views.html.index("Tableau de bord"))
   }
 
-  def form(url: String) = withAuth { username =>
+  /** edit given URI */
+  def form(uri: String) = withAuth { username =>
+    implicit request =>
+      println("editURI: " + request)
+      Ok(views.html.form(tableView.htmlForm(uri, editable = true
+      //        lang = chooseLanguage(request)
+      )))
+
+  }
+
+  /** create new instance of given class (unused) */
+  def create(url: String) = withAuth { username =>
     implicit request =>
       Ok(views.html.form(new CreationForm { actionURI = "/save" }.create(url, "en").get))
   }
 
-  def save = withAuth { username =>
+  /** */
+  def save(url: String) = withAuth { username =>
     implicit request =>
       val tableView = new TableView {}
-      Ok(views.html.report(tableView.htmlForm("http://xmlns.com/foaf/0.1", "", "").get))
+      Ok(views.html.report(tableView.htmlForm(url, "", "").get))
   }
 
 }
