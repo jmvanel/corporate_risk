cb3c69f9a4f6f01a65b988901af8502216b8df98
Sylvain <lebon.sylvain@gmail.com>
2015-01-23 17:02:11 +0100

    feature: save form ui: style fix

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,11 +3,14 @@ package controllers
 import play.api._
 import play.api.mvc._
 import deductions.runtime.html.{ CreationForm, TableView }
-import Auth._
+import deductions.runtime.services.FormSaver
+import deductions.runtime.sparql_cache.RDFCache
+import java.net.URLDecoder
 
 import models.UserData
+import Auth._
 
-object Application extends Controller with Secured {
+object Application extends Controller with Secured with RDFCache {
 
   def index = withUser { implicit user =>
     implicit request =>
@@ -16,13 +19,22 @@ object Application extends Controller with Secured {
 
   def form(url: String) = withUser { implicit user =>
     implicit request =>
-      Ok(views.html.form(new CreationForm { actionURI = "/save" }.create(url).get))
+      Ok(views.html.form(new CreationForm { actionURI = routes.Application.save.url }.create(url).get))
   }
 
   def save = withUser { implicit user =>
     implicit request =>
-      val tableView = new TableView {}
-      Ok(views.html.report(tableView.htmlForm("http://xmlns.com/foaf/0.1", "", "").get))
+      val uri = request.body match {
+        case form: AnyContentAsFormUrlEncoded =>
+          new FormSaver().saveTriples(form.data)
+          form.data.getOrElse("uri", Seq()).headOption match {
+            case Some(url1) => URLDecoder.decode(url1, "utf-8")
+            case _ => throw new IllegalArgumentException
+          }
+        case _ => throw new IllegalArgumentException
+      }
+
+      Ok(views.html.report(new TableView {}.htmlForm(uri).get))
   }
 
 }
