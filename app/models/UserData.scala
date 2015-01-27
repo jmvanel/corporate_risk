94356ef1599234b55bd2d7ecf53a34ad156a3d92
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2015-01-27 22:32:35 +0100

    UserData.scala : return type FormGroup; add contribution to FormGroup in N3 rule questionnaire2owl.q.n3
    TODO test Evaluation_capital_opérationnel.owl.ttl in app

--- app/models/UserData.scala
+++ app/models/UserData.scala
@@ -12,6 +12,8 @@ import deductions.runtime.jena.RDFStoreObject
 import deductions.runtime.abstract_syntax.UnfilledFormFactory
 import deductions.runtime.abstract_syntax.InstanceLabelsInference2
 import org.w3.banana.SparqlGraphModule
+import org.w3.banana.SparqlOpsModule
+import org.w3.banana.diesel._
 
 /** Banana principle: refer to concrete implementation only in blocks without code */
 object UserData extends RDFStoreLocalJena1Provider with UserDataTrait[Jena, Dataset]
@@ -20,7 +22,7 @@ trait UserDataTrait[Rdf <: RDF, DATASET] extends UserVocab
     with RDFStoreLocalProvider2[Rdf, DATASET]
     with InstanceLabelsInference2[Rdf]
     with SparqlGraphModule
-{
+    with SparqlOpsModule {
 
   import ops._
 
@@ -39,16 +41,17 @@ trait UserDataTrait[Rdf <: RDF, DATASET] extends UserVocab
     // TODO read RDF configuration for this, like is done for classes themselves 
     rdfStore.rw(
       dataset, {
-        for (classAndPropURI <- applicationClassesAndProperties())
+        for (classAndPropURI <- applicationClassesAndProperties().classesAndProperties )
           createEmptyClassInstanceForUser(getURI(user), classAndPropURI)
       })
   }
 
-  /** return a sequence of couples:
+  /**
+   * return a sequence of couples:
    * - an URI <u1> associated with the user <user> through one of the RDF properties <prop> in configuration :
    *     <user> <prop> <u1> .
    * - a label string associated to the class of <u1> in configuration.
-   * 
+   *
    * The configuration is gotten by function #applicationClassesAndProperties() .
    */
   def getUserData(user: User): Seq[(Rdf#URI, String)] = {
@@ -59,7 +62,7 @@ trait UserDataTrait[Rdf <: RDF, DATASET] extends UserVocab
         implicit val graphForVocabulary = rdfStore.getGraph(dataset,
           URI("vocabulary")).get
         for {
-          (cl, prop) <- applicationClassesAndProperties()
+          (cl, prop) <- applicationClassesAndProperties().classesAndProperties
           triple <- find(graph, userURI, prop, ANY)
         } yield {
           (triple.objectt, instanceLabel(cl))
@@ -73,18 +76,25 @@ trait UserDataTrait[Rdf <: RDF, DATASET] extends UserVocab
     uriOptions collect { case Some((uri, il)) => (uri, il) }
   }
 
-  /** return a sequence of URI couples:
+  /**
+   * return a sequence of URI couples:
    *  - an OWL class C,
    *  - and a property whose domain is :User and range C
-   *  
-   *  Since each C is associated to a form, this defines the top-level structure of the user data input. */
-  def applicationClassesAndProperties(formGroup:String="risk"): Seq[(Rdf#URI, Rdf#URI)] = {
+   *
+   *  Since each C is associated to a form, this defines the top-level structure of the user data input.
+   */
+  def applicationClassesAndProperties(formGroup: String = "risk"):
+//  Seq[(Rdf#URI, Rdf#URI)] 
+  FormGroup
+  = {
     formGroup match {
-      case "risk" => applicationClassesAndPropertiesRisk
-      case "nature" => applicationClassesAndPropertiesNature
-      case "company" => applicationClassesAndPropertiesCompany
-      case "brand" => applicationClassesAndPropertiesBrand
-      case _ => println( s"formGroup URI not expected: $formGroup" ); Seq((URI(""), URI("")))
+      case "risk" => FormGroup( applicationClassesAndPropertiesRisk,
+          "Questions sur la gestion des risques." )
+//      case "nature" => applicationClassesAndPropertiesNature
+//      case "company" => applicationClassesAndPropertiesCompany
+//      case "brand" => applicationClassesAndPropertiesBrand
+      case _ => println(s"formGroup URI not expected: $formGroup");
+      FormGroup( Seq((URI(""), URI(""))), "" )
     }
   }
 
@@ -100,30 +110,50 @@ trait UserDataTrait[Rdf <: RDF, DATASET] extends UserVocab
   }
 
   def applicationClassesAndPropertiesCompany(): Seq[(Rdf#URI, Rdf#URI)] = {
-		  applicationClassesAndPropertiesGeneric
-      Seq((URI(""), URI(""))) // TODO <<<<<<<<<<<
-  }
-    
-  def applicationClassesAndPropertiesBrand(): Seq[(Rdf#URI, Rdf#URI)] = {
-      Seq((URI(""), URI(""))) // TODO <<<<<<<<<<<
+    applicationClassesAndPropertiesGeneric
+    Seq((URI(""), URI(""))) // TODO <<<<<<<<<<<
   }
 
-  case class FormGroup(val classesAndProperties:Seq[(Rdf#URI, Rdf#URI)], label:String)
-  
-  /** Detect RDF patterns like:
+  def applicationClassesAndPropertiesBrand(): Seq[(Rdf#URI, Rdf#URI)] = {
+    Seq((URI(""), URI(""))) // TODO <<<<<<<<<<<
+  }
+
+  case class FormGroup(val classesAndProperties: Seq[(Rdf#URI, Rdf#URI)], label: String)
+
+  /**
+   * Detect RDF patterns like:
    * <pre>
    * :risk-fg a :FormGroup ;
-    rdfs:label "Questions sur la gestion des risques."@fr ;
-    :properties :p1, :p2 .
-     </pre>
-     * and return a list of couples (:p1, rdfs:range of :p1) . 
+   * rdfs:label "Questions sur la gestion des risques."@fr ;
+   * :properties :p1, :p2 .
+   * </pre>
+   * and return a list of couples (:p1, rdfs:range of :p1) .
    */
   def applicationClassesAndPropertiesGeneric(): Seq[FormGroup] = {
-      Seq(FormGroup( Seq((URI(""), URI(""))) , "" )) // TODO <<<<<<<<<<<
-      /*  
-  def executeSelect(a: A, query: Rdf#SelectQuery, bindings: Map[String, Rdf#Node]): M[Rdf#Solutions]
-  def executeConstruct(a: A, query: Rdf#ConstructQuery, bindings: Map[String, Rdf#Node]): M[Rdf#Graph]
-       */
+    val queryString = """
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      
+      SELECT ?LAB ?PROP ?CLASS
+      WHERE {
+      a :FormGroup ; rdfs:label ?LAB ;
+      :properties ?PROP .
+      ?PROP rdfs:range ?CLASS .
+      }
+    """
+    import sparqlOps._
+    val query = parseSelect(queryString).get
+    val solutions = rdfStore.executeSelect(dataset, query, Map()).get
+    val variables: Iterator[(Rdf#Literal, Rdf#URI, Rdf#URI)] = solutions.iterator map { row =>
+      /* row is an Rdf#Solution, we can get an Rdf#Node from the variable name
+       * both the #Rdf#Node projection and the transformation to Rdf#URI can fail
+       * in the Try type */
+      ( row("LAB"). get.as[Rdf#Literal].get,
+        row("CLASS").get.as[Rdf#URI].get,
+        row("PROP").get.as[Rdf#URI].get )        
+    }
+    println(variables.to[List].mkString("\n"))
+    val classesAndProperties = for ( v <- variables ) yield ( v._2, v._3 )
+    Seq(FormGroup( classesAndProperties.toSeq, fromLiteral( variables.next()._1 )._1 ))
   }
 
   private def createEmptyClassInstanceForUser(userURI: Rdf#URI, classAndPropURI: (Rdf#URI, Rdf#URI)) = {
