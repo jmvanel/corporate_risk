6756b1e79df0a1cb801491080b2f533f3236c9f5
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2015-02-05 14:14:38 +0100

    implement ResponseAnalysis.responsesCount() - CAUTION : added one argument

--- app/models/ResponseAnalysis.scala
+++ app/models/ResponseAnalysis.scala
@@ -1,22 +1,19 @@
 package models
 
-import org.w3.banana.jena.JenaModule
-import org.w3.banana.RDFOpsModule
-import deductions.runtime.jena.RDFStoreLocalProvider
-import deductions.runtime.jena.RDFStoreLocalJena1Provider
-import deductions.runtime.jena.RDFStoreLocalProvider2
+import scala.xml.Elem
+
 import org.w3.banana.RDF
-import org.w3.banana.jena.Jena
-import com.hp.hpl.jena.query.Dataset
-import deductions.runtime.jena.RDFStoreObject
-import deductions.runtime.abstract_syntax.UnfilledFormFactory
-import deductions.runtime.abstract_syntax.InstanceLabelsInference2
 import org.w3.banana.SparqlGraphModule
 import org.w3.banana.SparqlOpsModule
-import org.w3.banana.diesel._
-import org.apache.log4j.Logger
-import java.nio.file.StandardOpenOption
-import scala.xml.Elem
+import org.w3.banana.XSDPrefix
+import org.w3.banana.diesel.toPointedGraphW
+import org.w3.banana.jena.Jena
+
+import com.hp.hpl.jena.query.Dataset
+
+import deductions.runtime.abstract_syntax.InstanceLabelsInference2
+import deductions.runtime.jena.RDFStoreLocalJena1Provider
+import deductions.runtime.jena.RDFStoreLocalProvider2
 
 /** Responses Analysis */
 object ResponseAnalysis extends RDFStoreLocalJena1Provider with ResponseAnalysisTrait[Jena, Dataset]
@@ -27,11 +24,37 @@ trait ResponseAnalysisTrait[Rdf <: RDF, DATASET] extends UserVocab
     with SparqlGraphModule
     with SparqlOpsModule {
 
+  val xsd = XSDPrefix[Rdf]
   /**
    * fonction qui compte les réponses pour une propriété de User,
    *  c'est à dire un formulaire, alias une rubrique (alias thème)
    */
-  def responsesCount(propURI: String): Int = 0
+  def responsesCount(user: User, propURI: String): Int = {
+    val countTry = rdfStore.r(
+      dataset, {
+        val userURI = getURI(user)
+        // NOTE: could have used find() like in UserData.getUserData()
+        val queryString = s"""
+      SELECT DISTINCT (COUNT(?OBJ) AS ?count) 
+      WHERE {
+       GRAPH <$userURI> {
+         <$userURI> <$propURI> ?OBJ .
+       }
+      }
+    """
+        import sparqlOps._
+        import ops._
+        val query = parseSelect(queryString).get
+        val solutions = rdfStore.executeSelect(dataset, query, Map()).get
+        //     val variables: Iterator[(Rdf#Literal, Rdf#URI, Rdf#URI)] = solutions.iterator map { row =>
+        val res = solutions.iterator map { row =>
+          row("count").get.as[Rdf#Literal].get
+        }
+        res.next()
+      })
+    val lit = countTry.getOrElse(ops.makeLiteral("0", xsd.integer))
+    ops.fromLiteral(lit)._1.toInt
+  }
 
   /**
    * pour diagramme araignée, fonction qui chiffre chaque rubrique;
