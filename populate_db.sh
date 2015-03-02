1cdd314dd92004c1445763204f2186f3c960073b
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2015-03-02 12:08:13 +0100

    add vocabulary/risk/labels.ttl for #13

--- populate_db.sh
+++ populate_db.sh
@@ -3,6 +3,7 @@ echo "See also vocabulary/risk/README.md and vocabulary/capital/README.md"
 
 sbt <<EOF 
 runMain tdb.tdbloader --loc=TDB --graph=vocabulary vocabulary/risk/risk_questions.owl.ttl
+runMain tdb.tdbloader --loc=TDB --graph=vocabulary vocabulary/risk/labels.ttl
 runMain tdb.tdbloader --loc=TDB --graph=vocabulary vocabulary/capital/Evaluation_capital_opérationnel.owl.ttl
 runMain tdb.tdbloader --loc=TDB --graph=vocabulary vocabulary/capital/Evaluation_capital_Structurel.owl.ttl
 runMain tdb.tdbloader --loc=TDB --graph=vocabulary vocabulary/capital/Evaluation_du_capital_humain.owl.ttl
