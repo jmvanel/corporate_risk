87f84cf327f581608aee253cc6124e90b747fe9d
jmvanel <jeanmarc.vanel@gmail.com>
2016-08-04 12:29:30 +0200

    update_server_only_corporate_risk.sh ajouté lien ../TDBrisk3 pour mots de passe (mais n'est pas pris en compte car le stockage des mots de passe n'est pas comme semantic_forms)
    vocabulary/enterprise.owl.ttl : ajout secteur d'activité pour URI dbpedia.

--- update_server_only_corporate_risk.sh
+++ update_server_only_corporate_risk.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "update corporate_risk server when code has changed in corporate_risk"
+SRC=$HOME/src/corporate_risk/
+APP=corporate_risk
+APPVERS=${APP}-1.0-SNAPSHOT
 SBT=sbt
 
 cd ~/src/corporate_risk/
@@ -19,6 +22,11 @@ mv corporate_risk-1.0-SNAPSHOT  corporate_risk-1.0-SNAPSHOT_OLD
 unzip $HOME/src/corporate_risk/target/universal/corporate_risk-1.0-SNAPSHOT.zip
 
 cd corporate_risk-1.0-SNAPSHOT
-ln -s ../TDBrisk TDB
+mkdir -p ../TDBrisk
+mkdir -p ../TDBrisk2
+mkdir -p ../TDBrisk3
+
+ln -s ../TDBrisk  TDB
 ln -s ../TDBrisk2 TDB2
+ln -s ../TDBrisk3 TDB3
 nohup bin/corporate_risk -mem 100 -J-server -Dhttp.port=9153 &
